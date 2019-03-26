0c357a846121b42fb3fc4a35881294bf121234bc
edjunma <junwenma@buffalo.edu>
2019-03-26 01:55:47 -0400

    Adding background animation in body

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const rate = document.querySelector("#rate");
 const rateValue = document.querySelector("#rate-value");
 const pitch = document.querySelector("#pitch");
 const pitchValue = document.querySelector("#pitch-value");
+const body = document.querySelector("body");
 
 // Init voices array
 let voices = [];
@@ -37,6 +38,9 @@ if (synth.onvoiceschanged !== undefined) {
 
 // Speak
 const speak = () => {
+  // Add background animation
+  body.style.background = "#141414 url(../img/wave.gif)";
+  body.style.backgroundRepeat = "repeat-x";
   // Check if speaking
   if (synth.speaking) {
     console.error("Already speaking...");
