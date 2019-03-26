5367dd4014f8644a146396b91ad23509d0f53486
edjunma <junwenma@buffalo.edu>
2019-03-26 01:37:19 -0400

    Declared variables and DOM elements

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Init SpeechSynth API
+const synth = window.speechSynthesis;
+
+// DOM Elements
+const textForm = document.querySelector("form");
+const textInput = document.querySelector("#text-input");
+const voiceSelect = document.querySelector("#voice-select");
+const rate = document.querySelector("#rate");
+const rateValue = document.querySelector("#rate-value");
+const pitch = document.querySelector("#pitch");
+const pitchValue = document.querySelector("#pitch-value");
+
+// Init voices array
+let voices = [];
+
+const getVoices = () => {
+  voices = synth.getVoices();
+  console.log(voices);
+};
+
+getVoices();
